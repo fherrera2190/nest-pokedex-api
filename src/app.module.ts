03c34665b5953f22e5e0b8fd18bee30a2defab11
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-10-08 18:28:20 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    PokemonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
