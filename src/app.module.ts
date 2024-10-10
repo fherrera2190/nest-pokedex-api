bc967a939b08557db08db4879453ee4db78ed189
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-10-09 23:47:42 -0300

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    MongooseModule.forRoot('mongodb://localhost:27040/nest-pokemon'),
     PokemonModule,
   ],
   controllers: [],
